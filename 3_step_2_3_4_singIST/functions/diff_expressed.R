18d969a00ff372135601f779e8463fe05a479416
amoruno <109459201+amoruno@users.noreply.github.com>
2025-01-23 13:09:28 +0100

    Update diff_expressed.R

--- 3_step_2_3_4_singIST/functions/diff_expressed.R
+++ 3_step_2_3_4_singIST/functions/diff_expressed.R
@@ -1,12 +1,10 @@
 # Description: compute fold changes for disease model via FindMarkers
-diff_expressed <- function(SeuratObject, condition_1 = c(), condition_2 = c(), adjpval.treshold = 0.05,
-                           logfc.treshold = 0.05){
+diff_expressed <- function(SeuratObject, condition_1 = c(), condition_2 = c(),
+                           logfc.treshold = 0.25){
   # FindMarkers function by row of dataset
   apply_function <- function(row, data = SeuratObject) {
     logFC <- FindMarkers(object = data, ident.1 = row[1], ident.2 = row[2], 
                          assay = "RNA", slot = "data", logfc.threshold = logfc.treshold)
-    # Filter only genes with adjpval.treshold
-    logFC <- logFC[logFC$p_val_adj <= adjpval.treshold, ]
     return(logFC)
   }
   # Combinations to test
