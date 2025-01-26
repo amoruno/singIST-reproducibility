920ad68b12f42712c49c95d1d42f2be13ca1c07a
amoruno <109459201+amoruno@users.noreply.github.com>
2025-01-26 18:36:03 +0100

    Update figure_5.R

--- 4_graphical_output/scripts/figure_5.R
+++ 4_graphical_output/scripts/figure_5.R
@@ -9,7 +9,7 @@ df_FC <- do.call(rbind, lapply(Pathway_recap, function(x){
   )
 df_FC <- merge(df_pvalues, df_FC, by = "Pathway_name")
 
-save(df_gene, file = "C:/Users/amoruno/OneDrive - Almirall S.A/Doctorat/Publication 1/All pathways analysis/output_recapitulation/df_gene.RData")
+save(df_gene, file = "~/singIST-reproducibility/4_graphical_output/exported_results/df_gene.RData")
 save(df_FC, file = "C:/Users/amoruno/OneDrive - Almirall S.A/Doctorat/Publication 1/All pathways analysis/output_recapitulation/df_FC.RData")
 
 
